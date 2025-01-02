00eb34501bba296e454c6d899fba7038e0c8361b
Marisol <s.min.g7904@gmail.com>
2025-01-03 06:56:09 +0900

    Add swagger dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencyManagement {
 
 dependencies {
     // Spring
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
