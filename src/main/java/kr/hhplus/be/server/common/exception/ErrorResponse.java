c2c2c987ab00cfbba3e872fed87f403bb0b5b6b3
Marisol <s.min.g7904@gmail.com>
2025-01-10 09:38:41 +0900

    Add Global Exception Handler

---
+++ src/main/java/kr/hhplus/be/server/common/exception/ErrorResponse.java
@@ -0,0 +1,17 @@
+package kr.hhplus.be.server.common.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ErrorResponse {
+    private String code;
+    private String message;
+
+    public ErrorResponse(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+}
