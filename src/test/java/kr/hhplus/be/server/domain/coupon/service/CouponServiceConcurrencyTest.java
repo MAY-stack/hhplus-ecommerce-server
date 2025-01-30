cc607e5161f9725447925caafd329d9881fc772a
MarisoL <s.min.g7904@gmail.com>
2025-01-30 17:46:07 +0900

    remove ExtendWith

--- src/test/java/kr/hhplus/be/server/domain/coupon/service/CouponServiceConcurrencyTest.java
+++ src/test/java/kr/hhplus/be/server/domain/coupon/service/CouponServiceConcurrencyTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -24,7 +22,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @SpringBootTest
-@ExtendWith(MockitoExtension.class)
 class CouponServiceConcurrencyTest {
 
     @Autowired
